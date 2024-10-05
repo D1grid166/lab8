1566629931868e8c2f886ff8a14327ba6b5be3d6
Даниил Рюмшин <d1gridshint@yahoo.com>
2024-10-05 08:31:29 +0300

    time2

--- time.pas
+++ time.pas
@@ -1,15 +1,16 @@
 ﻿begin
   var a:=readinteger;
   if ((a mod 4) = 0) and ((a mod 100) <> 0) and ((a mod 400) <>0) then
-    println('366')
+    println('Високосный-366')
   else
-    println('365')
+    println('Обычный-365')
 end.
+//400
+//Обычный-365
 
-//333
-//365
+//196
+//Високосный-366
 
-//4
-//366
+
 
 
