8ddc20320af3956676d966111c14e7647fdba07b
Даниил Рюмшин <d1gridshint@yahoo.com>
2024-10-05 08:28:37 +0300

    time

---
+++ time.pas
@@ -0,0 +1,15 @@
+﻿begin
+  var a:=readinteger;
+  if ((a mod 4) = 0) and ((a mod 100) <> 0) and ((a mod 400) <>0) then
+    println('366')
+  else
+    println('365')
+end.
+
+//333
+//365
+
+//4
+//366
+
+
